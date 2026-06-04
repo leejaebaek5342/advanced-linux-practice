4ba9501efb20c7b6c73a5d484f8aceeaaad110fe
leejaebaek5342 <leejaebaek5342@gmail.com>
2026-06-04 14:29:24 +0900

    최종 결과물

---
+++ final_project/device/buzzer.c
@@ -0,0 +1,41 @@
+#include <wiringPi.h>
+#include <softTone.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define SPKR    3
+#define TOTAL   32
+
+extern volatile int buzzer_stop;
+
+int notes[] = {
+    391, 391, 440, 440, 391, 391, 329, 329,
+    391, 391, 329, 329, 293, 293, 293, 0,
+    391, 391, 440, 440, 391, 391, 329, 329,
+    391, 329, 293, 329, 261, 261, 261, 0
+};
+
+void buzzer_init()
+{
+    pinMode(SPKR, OUTPUT);
+}
+
+void buzzer_on()
+{
+    softToneCreate(SPKR);
+    for(int i = 0; i < TOTAL; i++)
+    {
+        if(buzzer_stop)
+            break;
+
+        softToneWrite(SPKR, notes[i]);
+        delay(280);
+    }
+    softToneWrite(SPKR, 0);
+}
+
+void buzzer_off()
+{
+    softToneWrite(SPKR, 0);
+}
